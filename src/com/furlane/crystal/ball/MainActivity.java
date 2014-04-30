6fff269ec1c810df6c8050565db4a6d1232936df
Anderson Furlane <andersonfurlanemartins@gmail.com>
2014-04-30 18:03:26 -0300

    finalização

---
+++ src/com/furlane/crystal/ball/MainActivity.java
@@ -0,0 +1,116 @@
+package com.furlane.crystal.ball;
+
+import android.app.Activity;
+import android.graphics.drawable.AnimationDrawable;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.animation.AlphaAnimation;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.furlane.crystal.ball.R;
+import com.furlane.crystal.ball.ShakeDetector.OnShakeListener;
+
+public class MainActivity extends Activity {
+	
+	private CrystalBall mCrystalBall = new CrystalBall();
+	private TextView mAnswerLabel;
+	private ImageView mCrystalBallImage;
+	private SensorManager mSensorManager;
+	private Sensor mAccelerometer;
+	private ShakeDetector mShakeDetector;
+	
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);		
+		// Assign the Views from the layout file
+		mAnswerLabel= (TextView) findViewById(R.id.textViewAnsewer);
+		mCrystalBallImage = (ImageView) findViewById(R.id.imageView1);
+		
+		mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+		mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		mShakeDetector = new ShakeDetector(new OnShakeListener() {
+			
+			@Override
+			public void onShake() {
+				handleNewAnswer();	
+			}
+		});
+	}
+	
+	@Override
+	public void onResume() {
+		super.onResume();
+		mSensorManager.registerListener(mShakeDetector, mAccelerometer, SensorManager.SENSOR_DELAY_UI);
+	}
+		
+	@Override
+	public void onPause() {
+		super.onPause();
+		mSensorManager.unregisterListener(mShakeDetector);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	private void handleNewAnswer() {		
+		// Update the label with our dynamic answer
+		mAnswerLabel.setText(mCrystalBall.getAnAnswer());
+		
+		animateCrystalBall();
+		animateAnswer();
+		playSound();
+	}
+	
+	private void animateAnswer() {
+		AlphaAnimation fadeInAnimation = new AlphaAnimation(0, 1);
+		fadeInAnimation.setDuration(1500);
+		fadeInAnimation.setFillAfter(true);
+		
+		mAnswerLabel.setAnimation(fadeInAnimation);
+	}
+	
+	private void animateCrystalBall() {
+		mCrystalBallImage.setImageResource(R.drawable.ball_animation);
+		AnimationDrawable ballAnimation = (AnimationDrawable) mCrystalBallImage.getDrawable();
+		
+		if (ballAnimation.isRunning()){
+			ballAnimation.stop();
+		}
+		
+		ballAnimation.start();
+	}
+	
+	private void playSound() {
+		MediaPlayer player = MediaPlayer.create(this, R.raw.crystal_ball);
+		player.start();
+		player.setOnCompletionListener(new OnCompletionListener() {
+			
+			@Override
+			public void onCompletion(MediaPlayer mp) {
+				mp.release();
+			}
+		});
+	}
+}
